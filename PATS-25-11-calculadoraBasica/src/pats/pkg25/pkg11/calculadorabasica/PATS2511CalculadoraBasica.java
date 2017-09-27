c040e49dc8a1d64e12af7bf00ea6a3e045725c1f
Edgardo Torres Vanden Bouch <edgardo.torres@hotmail.com>
2017-09-27 06:43:41 -0500

    archivos base

---
+++ PATS-25-11-calculadoraBasica/src/pats/pkg25/pkg11/calculadorabasica/PATS2511CalculadoraBasica.java
@@ -0,0 +1,24 @@
+/*
+Ejercicio 11: Construir un programa que simule el funcionamiento de una 
+calculadora, que pueda realizar las 4 operaciones aritmeticas básicas 
+(suma, resta, producto y división) con valores numericos enteros. El usuario 
+debe especificar la operacion con el primer carácter del primer parámetro de la 
+linea de comandos: S o s oara la suma, R o r para la resta, P, p, M o m para el 
+producto y D o d para la división.
+*/
+package pats.pkg25.pkg11.calculadorabasica;
+
+/**
+ *
+ * @author edgardo
+ */
+public class PATS2511CalculadoraBasica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
