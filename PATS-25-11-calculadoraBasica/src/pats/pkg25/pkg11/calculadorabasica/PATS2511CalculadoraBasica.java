c271b98395432d2924477f2d24980ffeaa79f58f
Edgardo Torres Vanden Bouch <edgardo.torres@hotmail.com>
2017-09-27 07:08:18 -0500

    creaos las variables, usamos switch en lugar de ifs

--- PATS-25-11-calculadoraBasica/src/pats/pkg25/pkg11/calculadorabasica/PATS2511CalculadoraBasica.java
+++ PATS-25-11-calculadoraBasica/src/pats/pkg25/pkg11/calculadorabasica/PATS2511CalculadoraBasica.java
@@ -8,17 +8,44 @@ producto y D o d para la división.
 */
 package pats.pkg25.pkg11.calculadorabasica;
 
-/**
- *
- * @author edgardo
- */
+import javax.swing.JOptionPane;
+
 public class PATS2511CalculadoraBasica {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int num1, num2,sum, res, mul, div;
+        char operacion;
+        
+        num1 = Integer.parseInt(JOptionPane.showInputDialog("Ingresa un NUMERO"));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog("Ingresa un OTRO NUMERO"));
+        operacion  = JOptionPane.showInputDialog("¿Que OPERACION quieres hacer?").charAt(0);
+        
+        switch(operacion){
+            case 's': case 'S':
+                sum = num1 + num2;
+                JOptionPane.showMessageDialog(null, "La SUMA es: " + sum);
+                break;
+            case 'r':
+            case 'R':
+                res = num1 - num2;
+                JOptionPane.showMessageDialog(null, "La RESTA es: " + res);
+                break;
+            case 'm':
+            case 'M':
+            case 'p':
+            case 'P':
+                mul = num1 * num2;
+                JOptionPane.showMessageDialog(null, "La MULTIPLICACION es: " + mul);
+                break;
+            case 'd': case 'D':
+                div = num1 / num2;
+                JOptionPane.showMessageDialog(null, "La DIVICION es: " + div);
+                break;
+            
+              
+        }
+        
     }
     
 }
